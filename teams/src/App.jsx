57c306aed168bfb4aafa4b8d16ce42d6c7dc0dbd
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-12-03 12:49:13 +0200

    Added interface

--- teams/src/App.jsx
+++ teams/src/App.jsx
@@ -4,7 +4,60 @@ function App() {
 
   return (
     <>
-      
+      <div className='members'>
+        <div className='member'>
+          <div className='icon'>КВ</div>
+          <div className='fio'>Ковальов Вадим Віталійович</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>ФВ</div>
+          <div className='fio'>Фатеев Руслан Викторович</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>ЧА</div>
+          <div className='fio'>Чмель Богдан Артемович</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>ЗА</div>
+          <div className='fio'>Зукуліс Катерина Анатоліївна</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>ЦД</div>
+          <div className='fio'>Цисар Андрій Дмитрович</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>ГВ</div>
+          <div className='fio'>Глоба Яна Вячеславівна</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>ДА</div>
+          <div className='fio'>Демшевська Емма Андріївна</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>ПС</div>
+          <div className='fio'>Присяжний Марк Сергійович</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>БС</div>
+          <div className='fio'>Брагін Олександр Сергійович</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>СС</div>
+          <div className='fio'>Столярова Анна Станіславівна</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>ШЮ</div>
+          <div className='fio'>Шевченко Володимир Юрійович</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>ЧС</div>
+          <div className='fio'>Чепурко Владислав Сергійович</div>
+        </div>
+        <div className='member'>
+          <div className='icon'>ОВ</div>
+          <div className='fio'>Ор'єв Максим Віталійович</div>
+        </div>
+      </div>
     </>
   )
 }
